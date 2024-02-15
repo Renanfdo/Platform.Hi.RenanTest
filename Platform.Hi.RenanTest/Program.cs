0acf802a53e4ac556e4dda07fe8dacfb772ca175
Renan Fiedler de Oliveira <renanfiedlerdeoliveira@gmail.com>
2024-02-15 11:04:27 -0300

    Respostas

--- Platform.Hi.RenanTest/Program.cs
+++ Platform.Hi.RenanTest/Program.cs
@@ -14,13 +14,14 @@ IHost host = Host.CreateDefaultBuilder().ConfigureServices(services =>
 }).Build();
 
 Console.WriteLine("1. ");
-Console.WriteLine(@"A: Depende o seu objetivo a ser alcançado, casos com interfaces você usa para garante que a classe que esta implementando, tenha a implementação de métodos e propriedades, assim garantindo
-que, quando utilizado/chamado para alguma requisição, tenha suas funcionalidades desenvolvidas. Ao adicionar injeção de bibliotecas, você pode compartilhar apenas a bibliteca que contém o contrato das requisições
-sem mesmo compartilhar a sua implementação. Para classes abstratas você também pode exigir a implementação de métodos e propriedades, podendo criar uma base destes métodos ja desenvolvida, podendo ou não ser sobrescrita
-ou sendo utilizada para base de método.");
-Console.WriteLine(@"B: Casos de herança são utilizado para reaproveitar dados e métodos, quando uma classe A herda de outra class B, tem o significado que a classe A contém os mesmos dados e espeficicações/caracteristicas
-que a classe B. Como por exemplo, a classe B seria uma classe para especificar as caracteristicas de Fruta, e a classe A seria especificação de uma Banana. Para delegação, dado o uso quando quando você quer especificar
-uma caracteristica que uma classe tem, um método/função em comum entre elas, mas não todas as caracteristicas. Diferença entre, banana é uma fruta, e banana tem função de descascar.");
+Console.WriteLine("A: Depende o seu objetivo a ser alcançado, casos com interfaces você usa para garante que a classe que esta implementando, tenha a implementação de métodos e propriedades, assim garantindo que, "
+    +"quando utilizado/chamado para alguma requisição, tenha suas funcionalidades desenvolvidas. Ao adicionar injeção de bibliotecas, você pode compartilhar apenas a bibliteca que contém o contrato das requisições "
+    +"sem mesmo compartilhar a sua implementação. Para classes abstratas você também pode exigir a implementação de métodos e propriedades, podendo criar uma base destes métodos ja desenvolvida, podendo ou não ser "
+    +"sobrescrita ou sendo utilizada para base de método.");
+Console.WriteLine(@"B: Casos de herança são utilizado para reaproveitar dados e métodos, quando uma classe A herda de outra class B, tem o significado que a classe A contém os mesmos dados e espeficicações/"
+    +"caracteristicas que a classe B. Como por exemplo, a classe B seria uma classe para especificar as caracteristicas de Fruta, e a classe A seria especificação de uma Banana. Para delegação, dado o uso "
+    +"quando quando você quer especificar uma caracteristica que uma classe tem, um método/função em comum entre elas, mas não todas as caracteristicas. Diferença entre, banana é uma fruta, e banana tem função "
+    +"de descascar.");
 
 Console.WriteLine(" ");
 Console.WriteLine("=================================================");
@@ -67,16 +68,16 @@ Console.WriteLine(" ");
 
 Console.WriteLine("4. ");
 Console.WriteLine(@"A: Sim, para todos os casos que você queira interpretar/especificar o erro que está acontecendo, assim a origem da requisição pode tratar diferentes tipos de erros de acordo com a Exception recebida.");
-Console.WriteLine(@"B: Quando você quer corrigir ou tratar o erro que você espera, para que o usuário tenha uma boa experiencia com o sistema sem transparecer erros sistemáticos. Como exemplo exceções de 
-infraestrutura/integrações, quando realizado uma integração onde o sistema destino não responde, deveria existir um tratativa de contorno para este erro.");
-Console.WriteLine(@"C: Lançada exceção quando o método ou código esta recebendo dados que não são coerentes ou não esperados, evitando que tenha erros na execução. Além da tratativa de entrada de dados, teremos 
-exceção para regras de negócio, para funcionalidade que não devem seguir em frente por alguma regra de negócio. Assim que não retornar exceção, o código/método terá sua execução completa com sucesso.");
+Console.WriteLine(@"B: Quando você quer corrigir ou tratar o erro que você espera, para que o usuário tenha uma boa experiencia com o sistema sem transparecer erros sistemáticos. Como exemplo exceções de "
+    +"infraestrutura/integrações, quando realizado uma integração onde o sistema destino não responde, deveria existir um tratativa de contorno para este erro.");
+Console.WriteLine(@"C: Lançada exceção quando o método ou código esta recebendo dados que não são coerentes ou não esperados, evitando que tenha erros na execução. Além da tratativa de entrada de dados, teremos "
+    +"exceção para regras de negócio, para funcionalidade que não devem seguir em frente por alguma regra de negócio. Assim que não retornar exceção, o código/método terá sua execução completa com sucesso.");
 
 Console.WriteLine(" ");
 Console.WriteLine("=================================================");
 Console.WriteLine(" ");
 
 Console.WriteLine("5. ");
-Console.WriteLine(@"Depende de cada situação e como esta sendo utilizado este web service, se as interfaces estão com injeção de Singleton, Transient ou Scoped. Para o EntityFrameWork você pode estar utilizando o 
-Tracking para bloqueio de registro, assim ficando bloqueado para sua sessão, assim que realizar o commit/saveChanges, o mesmo é liberado para outras sessões utilizar o registro. Em casos que não há tratativa, é 
-comun acontecer erro informando que o registro atual ou alterado por outro na requisição. Também nestes está faltando validação se a conta é existente ou não.");
+Console.WriteLine(@"Depende de cada situação e como esta sendo utilizado este web service, se as interfaces estão com injeção de Singleton, Transient ou Scoped. Para o EntityFrameWork você pode estar utilizando o "
+    +"Tracking para bloqueio de registro, assim ficando bloqueado para sua sessão, assim que realizar o commit/saveChanges, o mesmo é liberado para outras sessões utilizar o registro. Em casos que não há tratativa, é "
+    +"comum acontecer erro informando que o registro atual ou alterado por outro na requisição. Também nestes está faltando validação se a conta é existente ou não.");
